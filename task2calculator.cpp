e91d6559b3b28d3df52b3961429193ad7383c7a4
Muhammad Yousaf Naeem <yousafnaeem3388@gmail.com>
2024-04-26 21:02:52 +0500

    Add files via upload
    a simple calculator developed by using c++

---
+++ task2calculator.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    double num1, num2, result;
+    char operation;
+
+    
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> operation;
+
+
+    switch(operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                cout << "Error! Division by zero is not allowed." << endl;
+                return 1;
+            }
+            break;
+        default:
+            cout << "Invalid operation! Please choose from +, -, *, /." << endl;
+            return 1; 
+}
+
+    cout << "Result: " << result << endl;
+
+    return 0; 
+}
